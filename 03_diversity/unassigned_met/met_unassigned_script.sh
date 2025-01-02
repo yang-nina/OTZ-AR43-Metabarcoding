bbf2647149809fd373db87d693a72cb556157c80
Nina Yang <nina.yang0208@gmail.com>
2025-01-02 14:28:04 -0800

    Commit from RStudio

---
+++ 03_diversity/unassigned_met/met_unassigned_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Read header names from the input file into a variable
+header_names=$(cat met_unassigned.txt)
+
+# Initialize variables
+capturing=false
+
+# Loop through the FASTA file
+while IFS= read -r line; do
+    if [[ $line =~ ^\> ]]; then
+        # Check if the header is in the list of desired header names
+        if grep -q -w "${line:1}" <<< "$header_names"; then
+            capturing=true
+            echo "$line" >> filtered_met.fasta
+        else
+            capturing=false
+        fi
+    elif [ "$capturing" = true ]; then
+        echo "$line" >> filtered_met.fasta
+    fi
+done < reference.fasta
+
+echo "Filtered FASTA file generated."
