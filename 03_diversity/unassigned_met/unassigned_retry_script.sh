dba3fde1e4e1fa2b59b852cb75d1b74f41939633
Nina Yang <65185029+yang-nina@users.noreply.github.com>
2026-01-09 13:58:58 -0800

    Commit from RStudio

---
+++ 03_diversity/unassigned_met/unassigned_retry_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Read header names from the input file into a variable
+header_names=$(cat unassigned_retry.txt)
+
+# Initialize variables
+capturing=false
+
+# Loop through the FASTA file
+while IFS= read -r line; do
+    if [[ $line =~ ^\> ]]; then
+        # Check if the header is in the list of desired header names
+        if grep -q -w "${line:1}" <<< "$header_names"; then
+            capturing=true
+            echo "$line" >> filtered_unassigned_retry.fasta
+        else
+            capturing=false
+        fi
+    elif [ "$capturing" = true ]; then
+        echo "$line" >> filtered_unassigned_retry.fasta
+    fi
+done < /Users/ninayang/Documents/Git/OTZ-AR43-Metabarcoding/00_qiime2-outputs/18S/prdb-metazoa-ctd-rep-seqs.fasta
+
+echo "Filtered FASTA file generated."
